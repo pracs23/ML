c659225267bf1f4baffa5b18d22b0bc842de66ef
pracs23 <prachi.aug23@gmail.com>
2022-04-07 11:27:49 -0500

    Create part3.py

---
+++ PyTorch/part3.py
@@ -0,0 +1,166 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+
+# Define a transform to normalize the data
+transform = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                              ])
+# Download and load the training data
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+
+# Build a feed-forward network
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10))
+
+# Define the loss
+criterion = nn.CrossEntropyLoss()
+
+# Get our data
+images, labels = next(iter(trainloader))
+# Flatten images
+images = images.view(images.shape[0], -1)
+
+# Forward pass, get our logits
+logits = model(images)
+# Calculate the loss with the logits and the labels
+loss = criterion(logits, labels)
+
+print(loss)
+
+# Exercise: Build a model that returns the log-softmax as the output and calculate the loss using the negative log likelihood loss.
+## Solution
+
+# Build a feed-forward network
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+
+# Define the loss
+criterion = nn.NLLLoss()
+
+# Get our data
+images, labels = next(iter(trainloader))
+# Flatten images
+images = images.view(images.shape[0], -1)
+
+# Forward pass, get our log-probabilities
+logps = model(images)
+# Calculate the loss with the logps and the labels
+loss = criterion(logps, labels)
+
+x = torch.randn(2,2, requires_grad=True)
+print(x)
+
+y = x**2
+print(y)
+
+## grad_fn shows the function that generated this variable
+print(y.grad_fn)
+
+z = y.mean()
+print(z)
+
+print(x.grad)
+
+z.backward()
+print(x.grad)
+print(x/2)
+
+# Build a feed-forward network
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+logps = model(images)
+loss = criterion(logps, labels)
+
+print('Before backward pass: \n', model[0].weight.grad)
+
+loss.backward()
+
+print('After backward pass: \n', model[0].weight.grad)
+
+from torch import optim
+
+# Optimizers require the parameters to optimize and a learning rate
+optimizer = optim.SGD(model.parameters(), lr=0.01)
+
+print('Initial weights - ', model[0].weight)
+
+images, labels = next(iter(trainloader))
+images.resize_(64, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model.forward(images)
+loss = criterion(output, labels)
+loss.backward()
+print('Gradient -', model[0].weight.grad)
+
+# Take an update step and few the new weights
+optimizer.step()
+print('Updated weights - ', model[0].weight)
+
+model = nn.Sequential(nn.Linear(784, 128),
+                      nn.ReLU(),
+                      nn.Linear(128, 64),
+                      nn.ReLU(),
+                      nn.Linear(64, 10),
+                      nn.LogSoftmax(dim=1))
+
+criterion = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.003)
+
+epochs = 5
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # TODO: Training pass
+        optimizer.zero_grad()
+        
+        output = model.forward(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print(f"Training loss: {running_loss/len(trainloader)}")
+        
+        
+%matplotlib inline
+import helper
+
+images, labels = next(iter(trainloader))
+
+img = images[0].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logps = model.forward(img)
+
+# Output of the network are logits, need to take softmax for probabilities
+ps = torch.exp(logps)
+helper.view_classify(img.view(1, 28, 28), ps)
+
+
